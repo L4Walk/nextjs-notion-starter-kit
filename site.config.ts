8702bd281e6fe48c4148aacde867e517f4fc01b3
L4Walk <1005058349@qq.com>
2024-02-17 23:50:15 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '0685be34bc21488dacfb41568db5bd3a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'ScaleNav',
+  domain: 'tools.chuheng.tech',
+  author: 'L4Walk',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '发掘数据的价值',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
